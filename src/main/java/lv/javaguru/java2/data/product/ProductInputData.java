84f28b2248807f4bc16f11deb9efdb79324ed6f8
Dmitry Makeev <merecre@gmail.com>
2016-11-28 11:16:23 +0200

    Stores all Product input fields

---
+++ src/main/java/lv/javaguru/java2/data/product/ProductInputData.java
@@ -0,0 +1,17 @@
+package lv.javaguru.java2.data.product;
+
+/**
+ * Created by DMC on 11/28/2016.
+ */
+public class ProductInputData {
+
+    String productName;
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+}
