782863645dba39be428c22d362135d1d32c921f3
Dmitry Makeev <merecre@gmail.com>
2016-12-08 20:06:05 +0200

    Spring config added

---
+++ src/main/java/lv/javaguru/java2/config/SpringConfig.java
@@ -0,0 +1,13 @@
+package lv.javaguru.java2.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by DMC on 12/8/2016.
+ */
+
+@Configuration
+@ComponentScan(basePackages = {"lv.javaguru.java2"})
+public class SpringConfig {
+}
