0bee1da65a31407a9a048979704bd532bcf150bd
Dmitry Makeev <merecre@gmail.com>
2017-01-31 12:24:26 +0200

    Tests

--- src/test/java/lv/javaguru/java2/database/jdbc/DBUnitTestCase.java
+++ src/test/java/lv/javaguru/java2/database/jdbc/DBUnitTestCase.java
@@ -9,7 +9,7 @@ public abstract class DBUnitTestCase {
 
     @Before
     public void init() throws Exception {
-        databaseUtil.setupDatabaseFromFile(getDatabaseFile());
+        //databaseUtil.setupDatabaseFromFile(getDatabaseFile());
     }
 
     protected abstract String getDatabaseFile();
